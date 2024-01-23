7a93389b12ea58d8e42724c6cbf220f74b3ddd71
Henry Catalini Smith <henry@catalinismith.com>
2024-01-23 15:12:09 +0100

    Set up a video on a separate screen

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Video } from "expo-av";
+import { Button, Dimensions, SafeAreaView } from "react-native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Button
+        title="Video"
+        onPress={() => {
+          navigation.navigate("Video");
+        }}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function VideoScreen({ navigation }) {
+  return (
+    <SafeAreaView>
+      <Video
+        autoPlay
+        useNativeControls
+        source={require("./assets/hams.mp4")}
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").width * 0.56,
+        }}
+      />
+      <Button
+        title="Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Video" component={VideoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
